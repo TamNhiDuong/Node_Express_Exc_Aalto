d9d235b028853ca5ccd3842155b89d66918df809
TamNhiDuong <yanxinyee@gmail.com>
2023-03-24 12:28:04 +0200

    Exc 3.2 Return HTML elements by server

--- index.js
+++ index.js
@@ -2,42 +2,44 @@ const express = require('express')
 const app = express()
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    { 
-        "id": 5,
-        "name": "Test User", 
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
         "number": "39-23-6423122"
     }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/info', (request, response) => {
+    const personsCount = persons.length
+    const currentTime = new Date()
+    response.send(
+        `<div>
+            <p>Phonebook has info for ${personsCount} people</p>
+            <p>${currentTime}</p>
+        </div>`
+    )
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    response.json(persons)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })

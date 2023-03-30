5917fd6f9816725800ebbaba5ae5d23e80baed12
TamNhiDuong <yanxinyee@gmail.com>
2023-03-30 13:56:22 +0300

    Exc 3.9 Added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 
@@ -8,6 +9,7 @@ const app = express()
 app.use(express.json())
 //app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(cors())
 
 let persons = [
     {
